2d692eca726fc28850c40dd3236ff14b8c7d2355
Nardin Lachowski <nardin@live.com>
2021-07-28 15:37:33 -0400

    added image of running app, updated the readme file, completed the logic of the auto generated selection that appears for the users

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Rock Paper Scissors - Simple Game built with JS and React
+
+Rock Paper Scissors is a hand game usually played between two people. Each player simultaneously forms one of three shapes with their hand. In this case, you click your selection and the app automatically generates its selection randomly. Please enjoy and feel free to reference it as you build yours!
+
+Screenshot of application:
+![Screenshot](./rockpaperscissors.png)
+
+Live link, [here](https://nard1n.github.io/rock-paper-scissors)
+
+## Table of Contents
+
+* [Installation](#Installation)
+* [Usage](#Usage)
+* [Contributing](#Contributing)
+* [Tests](#Tests)
+* [License](#License)
+* [Questions](#Questions)
+
+## Installation
+Feel free to clone the repo or reference the code and simply run it in your browser
+
+## Usage
+For practicing Javascript and React (switch cases, event listeners, useState and useEffect hooks)
+
+## Contributing
+We love seeing community contribution to any opensource project! If you would like to contribute, please do.
+
+## Tests
+n/a
+## License
+This project is released under MIT opensource license:
+
+https://opensource.org/licenses/MIT
+
+## Questions
+For more about my work, check out my Github profile: https://github.com/nard1n
+
+If you have any questions and would like to chat, please feel free to send me an email directly to nardincodes@gmail.com
--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const App = () => {
-  const[userChoice, setUserChoice] = useState(null);
-  const choices = ['rock', 'paper', 'scissors'];
+  const [userChoice, setUserChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+  const [result, setResult] = useState(null);
+
+  const choices = ['Rock', 'Paper', 'Scissors'];
 
   const handleClick =(value) => {
-    setUserChoice(value)
-
+    setUserChoice(value);
+    generateComputerChoice();
   }
+
+  const generateComputerChoice = () => {
+    const randomNum = [Math.floor(Math.random() * choices.length)];
+    const randomChoice = choices[randomNum];
+    setComputerChoice(randomChoice);
+  }
+
+  useEffect(() => {
+    {
+      switch(userChoice + computerChoice) {
+        case 'ScissorsPaper':
+        case 'RockScissors':
+        case 'PaperRock':
+            setResult('YOU WIN!');
+            break;
+        case 'PaperScissors':
+        case 'ScissorsRock':
+        case 'RockPaper':
+          setResult('YOU LOSE!');
+          break;
+        case 'RockRock':
+        case 'PaperPaper':
+        case 'ScissorsScissors':
+          setResult("It's a draw. Try again!");
+          break;
+      }
+    }
+  }, [computerChoice, userChoice]);
 
   return (
     <div>
       <h2>Rock Paper Scissors</h2>
       <hr /> 
-      <h1>User Choice Is: {userChoice}</h1>
-      <h1>Computer Choice Is:</h1>
+      <h1>User Choice Is: <br/>
+        <span style={{color: "cadetblue"}}>{userChoice}</span>
+      </h1>
+      <h1>Computer Choice Is: <br/>
+        <span style={{color: "cadetblue"}}>{computerChoice}</span>
+      </h1>
         {choices.map((choice, index) => 
           <button key={index} onClick={()=> handleClick(choice)}>{choice}</button>
         )}
+        <h1 style={{color: "tan"}}>{result}</h1>
     </div>
   );
 }
