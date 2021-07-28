afc4c0361926fd42ed05e9b46a2dce7500986333
Nardin Lachowski <nardin@live.com>
2021-07-28 11:47:45 -0400

    added user choice to populate button click, updated app name and header section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useState } from 'react';
+
 const App = () => {
+  const[userChoice, setUserChoice] = useState(null);
+  const choices = ['rock', 'paper', 'scissors'];
+
+  const handleClick =(value) => {
+    setUserChoice(value)
+
+  }
+
   return (
     <div>
-      <h2>Rock Paper Scissors</h2> 
+      <h2>Rock Paper Scissors</h2>
+      <hr /> 
+      <h1>User Choice Is: {userChoice}</h1>
+      <h1>Computer Choice Is:</h1>
+        {choices.map((choice, index) => 
+          <button key={index} onClick={()=> handleClick(choice)}>{choice}</button>
+        )}
     </div>
   );
 }
